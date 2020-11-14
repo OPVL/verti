ab3ed4f768eeeb528313c385e69f1c6d51b923a1
OPVL <lloydculpepper4@gmail.com>
2020-11-14 23:09:16 +0000

    lint

--- app/Http/Controllers/TimeLogController.php
+++ app/Http/Controllers/TimeLogController.php
@@ -2,6 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+
 class TimeLogController extends Controller
 {
+    public function store(Request $request): RedirectResponse
+    {
+        return redirect()->back();
+    }
 }
